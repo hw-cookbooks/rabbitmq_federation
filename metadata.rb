6d8b26f605ae203c0050d555d99e330e36b4a074
Sean Porter <portertech@gmail.com>
2015-01-26 13:57:37 -0800

    cb create

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'rabbitmq_federation'
+maintainer       'Heavy Water Operations, LLC.'
+maintainer_email 'support@hw-ops.com'
+license          'Apache 2.0'
+description      'Installs/Configures rabbitmq_federation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
